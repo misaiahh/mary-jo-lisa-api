4de4884546e78d05413b4b353aa5ff35d68bd052
mickersicc <mickersicc@gmail.com>
2022-10-29 20:02:45 -0400

    added ping websocket

--- apps/auth/src/auth.controller.ts
+++ apps/auth/src/auth.controller.ts
@@ -1,4 +1,11 @@
-import { Controller, Get, Request, Post, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Request,
+  Post,
+  UseGuards,
+  Logger,
+} from '@nestjs/common';
 
 import { AuthService } from './auth.service';
 import { JwtAuthGuard } from './guards/jwt-auth.guard';
@@ -8,15 +15,27 @@ import { LocalAuthGuard } from './guards/local-auth.guard';
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  private readonly logger = new Logger(AuthController.name);
+
   @UseGuards(LocalAuthGuard)
   @Post('auth/login')
-  async login(@Request() req) {
+  async login(@Request() req): Promise<{ access_token: string }> {
+    const {
+      user: { userId, username },
+    } = req;
+    this.logger.log('[login]', userId, username);
+
     return this.authService.login(req.user);
   }
 
   @UseGuards(JwtAuthGuard)
   @Get('profile')
-  getProfile(@Request() req) {
+  getProfile(@Request() req): any {
+    const {
+      user: { userId, username },
+    } = req;
+    this.logger.log('[getProfile]', userId, username);
+
     return req.user;
   }
 }
