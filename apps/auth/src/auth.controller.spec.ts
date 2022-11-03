bbdcb1f83a1fd22cc877c9d8b3e647cdfaf37a0a
misaiahh <108594521+misaiahh@users.noreply.github.com>
2022-11-03 14:36:47 -0400

    fixed auth app unit tests (#5)
    Co-authored-by: mickersicc <mickersicc@gmail.com>

--- apps/auth/src/auth.controller.spec.ts
+++ apps/auth/src/auth.controller.spec.ts
@@ -1,12 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
+
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { UsersService } from './users/users.service';
+import { JwtModule, JwtService } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { UsersModule } from './users/users.module';
 
 describe('AuthController', () => {
   let authController: AuthController;
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
+      imports: [
+        UsersModule,
+        PassportModule,
+        JwtModule.register({
+          secret: 'test',
+          signOptions: { expiresIn: '60s' },
+        }),
+      ],
       controllers: [AuthController],
       providers: [AuthService],
     }).compile();
@@ -14,9 +27,32 @@ describe('AuthController', () => {
     authController = app.get<AuthController>(AuthController);
   });
 
-  describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(authController.getHello()).toBe('Hello World!');
+  describe('getProfile', () => {
+    it('should return a user object', () => {
+      const request = {
+        user: {
+          userId: 1,
+          username: 'john',
+        },
+      };
+      const response = {
+        userId: 1,
+        username: 'john',
+      };
+      expect(authController.getProfile(request)).toStrictEqual(response);
+    });
+  });
+
+  describe('login', () => {
+    it('should return an object with an "access_token"', async () => {
+      const request = {
+        user: {
+          userId: 1,
+          username: 'john',
+        },
+      };
+      const result = await authController.login(request);
+      expect(result.access_token).toBeTruthy();
     });
   });
 });
--- apps/auth/src/auth.service.ts
+++ apps/auth/src/auth.service.ts
@@ -14,6 +14,7 @@ export class AuthService {
     const user = await this.userService.findOne(username);
 
     if (user && user.password == pass) {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
       const { password, ...result } = user;
       return result;
     }
