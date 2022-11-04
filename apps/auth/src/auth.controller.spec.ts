210af620aaaeb79614f4886769b1b84de596f9d7
mickersicc <mickersicc@gmail.com>
2022-11-04 14:07:08 -0400

    fixed auth e2e tests

--- apps/auth/src/auth.controller.spec.ts
+++ apps/auth/src/auth.controller.spec.ts
@@ -1,10 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { UsersService } from './users/users.service';
-import { JwtModule, JwtService } from '@nestjs/jwt';
-import { PassportModule } from '@nestjs/passport';
 import { UsersModule } from './users/users.module';
 
 describe('AuthController', () => {
