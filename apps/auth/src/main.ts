d40b2ae33327667a67b142ecb3f16cb1f95106d0
misaiahh <108594521+misaiahh@users.noreply.github.com>
2022-11-09 12:15:38 -0500

    added cors to localhost (#7)
    Co-authored-by: mickersicc <mickersicc@gmail.com>

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -5,6 +5,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AuthModule, {
     logger: ['debug', 'error', 'log', 'verbose', 'warn'],
   });
+  app.enableCors({
+    origin: ['http://localhost:5173'],
+    methods: ['GET', 'POST'],
+    credentials: true,
+  });
   await app.listen(3001);
 }
 bootstrap();
